fcf187251bcf1b465c0fb07787232d349aa71f73
asad.balach@systemsltd.com <asadalibaloch059@gmail.com>
2020-09-08 14:17:49 +0500

    saving file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 const app = express();
 var multer = require('multer');
-var upload = multer({dest:'uploads/'});
+var upload = multer({ storage: storage })
 
 const port = 5000;
 
@@ -12,10 +12,28 @@ app.get('/', (req, res) => {
 app.post('/single', upload.single('profile'), (req, res) => {
     try {
       res.send(req.file);
-    }catch(err) {
+    } catch(err) {
       res.send(400);
     }
-  });
+});
+
+app.post('/bulk', upload.array('profiles', 4) , (req, res) =>{
+    try {
+        res.send(req.files);
+    } catch(error) {
+        console.log(error);
+        res.send(400);
+    }
+});
+
+var storage = multer.diskStorage({
+    dest: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null , file.originalname);
+    }
+});
 
 app.listen(port, () => {
     console.log("listening to the port: " + port);
